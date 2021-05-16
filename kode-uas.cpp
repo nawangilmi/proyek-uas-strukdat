9b43a1ded0faf1b14bfcf1949297553a4d5fde61
nawangilmi <nawangilmi003@gmail.com>
2021-05-16 12:07:37 +0700

    udah ngerjain switchnyaaa hehe

--- kode-uas.cpp
+++ kode-uas.cpp
@@ -1,16 +1,66 @@
+/* 
+
+
+
+
+
+
+
+*/
+
+
+
+
 #include <iostream>
 
 struct pemasukkan{
     int uangmasuk;
-
+    char tanggal[15];
 };
+struct pengeluaran{
+    int uangkeluar;
+    char tanggal[15];
+};
+
+typedef pemasukkan* pMasuk;
+typedef pengeluaran* pKeluar;
+
+void InputPemasukkan(){
+
+}
+
+void InputPengeluaran(){
+
+}
+
+void DataKeuangan(){
+    int pilih;
+    std::cout << "=== Data Keuangan ===" << "\n";
+    std::cout << "1. Rata-rata Pemasukkan\n";
+    std::cout << "2. Rata-rata Pengeluaran\n";
+    std::cout << "3. Pemasukkan Terbesar\n";
+    std::cout << "4. Pengeluaran Terbesar\n";
+    std::cout << "5. \n";
+    std::cout << "6. \n";
+    std::cout << "Masukkan Pilihan :";
+    std::cin >> pilih;
+
+    switch(pilih){
+        case 1 :
+            std::cout << "dadadsd";
+            break;
+        case 2 :
+            std::cout << "dadadsd";
+            break;
+        
+    }
+
+}
 
 int main(){
     int pilih;
-
-
-
-
+    do{
+    system("cls");
     std::cout << "==================================\n";
     std::cout << "           BUDGETIN AJA           \n";
     std::cout << "==================================\n";
@@ -20,4 +70,26 @@ int main(){
     std::cout << "4. Data - data Keuangan           \n";
     std::cout << "Masukkan Pilihan :";
     std::cin >> pilih;
+
+    switch (pilih)
+    {
+    case 1:
+        InputPemasukkan();
+        break;
+    case 2:
+        InputPengeluaran();
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        DataKeuangan();
+        break;
+
+    default:
+        break;
+    }
+    std::cout<<std::endl;
+		system("pause");
+    } while (pilih!=5);
 }
